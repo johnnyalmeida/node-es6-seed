59ef4d79cd6e69bcac689bd91b43768de18b4276
Henrique Rieger <henrique502@users.noreply.github.com>
2017-01-19 13:39:36 -0200

    Update database config
    Adicionado parâmetros de conexão

--- src/config/db.js
+++ src/config/db.js
@@ -3,12 +3,17 @@ require('dotenv').config();
 
 /* DB Config */
 const connection = require('knex')({
-  client: 'mysql',
+  client: 'mysql2',
   connection: {
     host: process.env.DB_HOST,
     user: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
+    supportBigNumbers: true,
+    bigNumberStrings: true,
+    multipleStatements: true,
+    timezone: 'UTC',
+    dateStrings: true
   },
   pool: { min: process.env.DB_POOL_MIN, max: process.env.DB_POOL_MAX },
 });
