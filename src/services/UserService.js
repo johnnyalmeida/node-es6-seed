a94fef97986bd2dcef0cd12ec58c9c2779a2a5f0
Luan Motta <luanmotta97@hotmail.com>
2017-03-31 08:46:52 -0300

    feat: Add all user crud methods

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,10 +1,56 @@
 const UserModel = require('../models/UserModel');
-const knex = require('../config/db');
+const Convert = require('../helpers/conversion');
 
 class UserService {
 
   static list(knex) {
-    return UserModel.list(knex);      
+    return UserModel.list(knex)
+    .then((dbList) => {
+      if (dbList.length === 0) {
+        return dbList;
+      }
+
+      const result = dbList.map(user => ({
+        id: user.id,
+        name: user.name,
+        status: user.status,
+        createdAt: Convert.toUnixEpoch(user.created_at),
+        updatedAt: Convert.toUnixEpoch(user.updated_at),
+        deletedAt: Convert.toUnixEpoch(user.deleted_at),
+      }));
+      return result;
+    });
+  }
+
+  static get(data, knex) {
+    return UserModel.get(data, knex)
+      .then((dbUser) => {
+        if (!dbUser[0]) {
+          return null;
+        }
+
+        const result = {
+          id: dbUser[0].id,
+          name: dbUser[0].name,
+          status: dbUser[0].status,
+          createdAt: Convert.toUnixEpoch(dbUser[0].created_at),
+          updatedAt: Convert.toUnixEpoch(dbUser[0].updated_at),
+          deletedAt: Convert.toUnixEpoch(dbUser[0].deleted_at),
+        };
+        return result;
+      });
+  }
+
+  static post(data, knex) {
+    return UserModel.post(data, knex);
+  }
+
+  static put(data, knex) {
+    return UserModel.put(data, knex);
+  }
+
+  static delete(data, knex) {
+    return UserModel.delete(data, knex);
   }
 }
 
